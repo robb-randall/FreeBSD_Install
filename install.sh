dd789c8801eba19ce61b20f537e49a41f79b6727
Robb Randall <robb@godaddy.com>
2020-06-02 10:53:32 -0500

    added disable beeps

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ sed -i '' "s/quarterly/latest/g" /etc/pkg/FreeBSD.conf
 ### Bootstrap pkg
 pkg update
 
+### Disable beeps
+sysrc allscreens_kbdflags="-b quiet.off"
+
 ### Setup wifi (rtwn)
 sysrc -f /boot/loader.conf if_rtwn_pci_load="YES"
 echo 'legal.realtek.license_ack=1' >> /boot/loader.conf
@@ -42,4 +45,4 @@ echo 'kern.vty=vt' >> /boot/loader.conf
 pkg install -y i3-gaps i3blocks i3lock i3status conky dmenu
 
 ### Install Software
-pkg install -y chromium git  gnome-screenshot nitrogen vscode xterm
+pkg install -y chromium git gnome-screenshot luit nitrogen vscode xterm
