b55aab313029318085071f6f0f7b1de048b82ad5
Robb Randall <rxrandall@godaddy.com>
2020-06-25 14:09:50 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,20 +1,51 @@
 ### Update FreeBSD
 freebsd-update fetch install
 
-### Change pkg from Quarterly to Latest
-sed -i '' "s/quarterly/latest/g" /etc/pkg/FreeBSD.conf
 
-### Bootstrap pkg
-pkg update
+### Update /etc/rc.conf
+sysrc clear_tmp_enable="YES"
+sysrc syslogd_flags="-ss"
+sysrc sendmail_enable="NONE"
+sysrc hostname="robb-laptop"
+sysrc local_unbound_enable="YES"
+sysrc sshd_enable="YES"
+sysrc moused_enable="YES"
+sysrc ntpdate_enable="YES"
+sysrc ntpd_enable="YES"
+sysrc powerd_enable="YES"
+sysrc dumpdev="NO"
 
-### Disable beeps
 sysrc allscreens_kbdflags="-b quiet.off"
 
-### Setup wifi (rtwn)
-sysrc -f /boot/loader.conf if_rtwn_pci_load="YES"
-echo 'legal.realtek.license_ack=1' >> /boot/loader.conf
 sysrc wlans_rtwn0="wlan0"
 sysrc ifconfig_wlan0="WPA SYNCDHCP"
+
+sysrc kld_list="/boot/modules/i915kms.ko"
+
+sysrc firewall_enable="YES"
+sysrc firewall_type="workstation"
+sysrc firewall_allowservices="any"
+sysrc firewall_myservices="22/tcp 80/tcp"
+
+sysrc dbus_enable="YES"
+sysrc hald_enable="YES"
+sysrc gdm_enable="YES"
+sysrc gnome_enable="YES"
+
+sysrc linux_enable="YES"
+kldload linux64
+
+
+### Update /boot/loader.conf
+echo 'kern.vty=vt' >> /boot/loader.conf
+
+echo 'if_rtwn_pci_load="YES"' >> /boot/loader.conf
+echo 'legal.realtek.license_ack=1' >> /boot/loader.conf
+
+echo 'hw.psm.synaptics_support="1"' >> /boot/loader.conf
+
+
+### Create /etc/wpa_supplicant.conf
 touch /etc/wpa_supplicant.conf
 cat > /etc/wpa_supplicant.conf <<EOL
 network={
@@ -23,23 +54,54 @@ network={
 }
 EOL
 
-### Setup Intel Graphics
-pkg install -y drm-kmod
-sysrc kld_list="/boot/modules/i915kms.ko"
 
-### Firewall (IPFW)
-sysrc firewall_enable="YES"
-sysrc firewall_type="workstation"
-sysrc firewall_allowservices="any"
-sysrc firewall_myservices="22/tcp 80/tcp"
-service ipfw start
-
-### Automount
+### Bootstrap pkg
+sed -i '' "s/quarterly/latest/g" /etc/pkg/FreeBSD.conf
+pkg update
 
 
-### Install and configure Xorg, Slim, and i3
-pkg install -y xorg xf86-video-intel
-echo 'kern.vty=vt' >> /boot/loader.conf
+### Install Packages (pkg prime-origins)
+pkg install -y \
+    devel/autoconf \
+    devel/automake \
+    sysutils/automount \
+    shells/bash \
+    devel/bison \
+    sysutils/debootstrap \
+    graphics/drm-kmod \
+    devel/geany \
+    devel/geany-plugin-spellcheck \
+    devel/geany-plugin-treebrowser \
+    devel/geany-plugin-workbench \
+    devel/geany-plugins \
+    devel/geany-themes \
+    devel/gettext-tools \
+    devel/git \
+    deskutils/gnome-shell-extra-extensions \
+    x11/gnome3 \
+    www/iridium \
+    editors/libreoffice \
+    devel/libtool \
+    emulators/linux_base-c7 \
+    security/sudo \
+    editors/vim \
+    editors/vscode \
+    net-mgmt/wifimgr \
+    x11-drivers/xf86-video-intel \
+    x11/xorg-minimal
 
-### Install DE and Programs
-pkg install -y i3-gaps i3blocks i3lock i3status dmenu  chromium git gnome-screenshot nitrogen vscode sakura vim
+
+### Update /etc/fstab
+echo 'linprocfs   /compat/linux/proc  linprocfs       rw      0       0' >> /etc/fstab
+echo 'linsysfs    /compat/linux/sys   linsysfs        rw      0       0' >> /etc/fstab
+echo 'tmpfs    /compat/linux/dev/shm  tmpfs   rw,mode=1777    0       0' >> /etc/fstab
+mount -a
+
+
+# Update robb's user
+pw group mod wheel -m robb
+pw group mod video -m robb
+pw group mod operator -m robb
+
+chsh -s /usr/local/bin/bash robb
+echo 'robb ALL=(ALL) NOPASSWD: ALL' >> /usr/local/etc/sudoers
