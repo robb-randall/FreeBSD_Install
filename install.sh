0cdcf631531c92e3d912edd6e152c32f9d7987db
Robb Randall <rxrandall@godaddy.com>
2020-06-29 09:35:18 -0500

    refactored

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-### Update FreeBSD
-freebsd-update fetch install
+### Update generic /boot/loader.conf settings
+sysrc -f /boot/loader.conf loader_logo="beastie"
+sysrc -f /boot/loader.conf autoboot_delay=2
+sysrc -f /boot/loader.conf loader_delay=2
 
-
-### Update /etc/rc.conf
+### Update generic /etc/rc.conf settings
 sysrc clear_tmp_enable="YES"
 sysrc syslogd_flags="-ss"
 sysrc sendmail_enable="NONE"
-sysrc hostname="robb-laptop"
 sysrc local_unbound_enable="YES"
 sysrc sshd_enable="YES"
 sysrc moused_enable="YES"
@@ -15,37 +15,15 @@ sysrc ntpd_enable="YES"
 sysrc powerd_enable="YES"
 sysrc dumpdev="NO"
 
+### Disable beeps
 sysrc allscreens_kbdflags="-b quiet.off"
+kbdcontrol -b off
 
+### Setup Wifi
+sysrc -f /boot/loader.conf if_rtwn_pci_load="YES"
+echo 'legal.realtek.license_ack=1' >> /boot/loader.conf
 sysrc wlans_rtwn0="wlan0"
 sysrc ifconfig_wlan0="WPA SYNCDHCP"
-
-sysrc kld_list="/boot/modules/i915kms.ko"
-
-sysrc firewall_enable="YES"
-sysrc firewall_type="workstation"
-sysrc firewall_allowservices="any"
-sysrc firewall_myservices="22/tcp 80/tcp"
-
-sysrc dbus_enable="YES"
-sysrc hald_enable="YES"
-sysrc gdm_enable="YES"
-sysrc gnome_enable="YES"
-
-sysrc linux_enable="YES"
-kldload linux64
-
-
-### Update /boot/loader.conf
-echo 'kern.vty=vt' >> /boot/loader.conf
-
-echo 'if_rtwn_pci_load="YES"' >> /boot/loader.conf
-echo 'legal.realtek.license_ack=1' >> /boot/loader.conf
-
-echo 'hw.psm.synaptics_support="1"' >> /boot/loader.conf
-
-
-### Create /etc/wpa_supplicant.conf
 touch /etc/wpa_supplicant.conf
 cat > /etc/wpa_supplicant.conf <<EOL
 network={
@@ -54,21 +32,54 @@ network={
 }
 EOL
 
+### Update FreeBSD
+freebsd-update fetch install
 
 ### Bootstrap pkg
 sed -i '' "s/quarterly/latest/g" /etc/pkg/FreeBSD.conf
 pkg update
 
+### Install Intel graphics
+pkg install -y graphics/drm-kmod
+sysrc kld_list="/boot/modules/i915kms.ko"
 
-### Install Packages (pkg prime-origins)
+### Install Gnome
+pkg install -y \
+    deskutils/gnome-shell-extra-extensions \
+    x11/gnome3 \
+    x11-drivers/xf86-video-intel \
+    x11/xorg-minimal
+echo 'kern.vty=vt' >> /boot/loader.conf
+echo 'hw.psm.synaptics_support="1"' >> /boot/loader.conf
+sysrc dbus_enable="YES"
+sysrc hald_enable="YES"
+sysrc gdm_enable="YES"
+sysrc gnome_enable="YES"
+
+### Setup Firewall
+sysrc firewall_enable="YES"
+sysrc firewall_type="workstation"
+sysrc firewall_allowservices="any"
+sysrc firewall_myservices="22/tcp 80/tcp"
+
+### Setup Linux Compatibility
+pkg install -y emulators/linux_base-c7
+sysrc linux_enable="YES"
+sysrc fdescfs_load="YES"
+kldload linux64
+echo 'linprocfs   /compat/linux/proc  linprocfs       rw      0       0' >> /etc/fstab
+echo 'linsysfs    /compat/linux/sys   linsysfs        rw      0       0' >> /etc/fstab
+echo 'tmpfs    /compat/linux/dev/shm  tmpfs   rw,mode=1777    0       0' >> /etc/fstab
+echo 'fdescfs /dev/fd  fdescfs  rw  0  0' >> /etc/fstab
+mount -a
+
+### Install software (pkg prime-origins)
 pkg install -y \
     devel/autoconf \
     devel/automake \
     sysutils/automount \
     shells/bash \
     devel/bison \
-    sysutils/debootstrap \
-    graphics/drm-kmod \
     devel/geany \
     devel/geany-plugin-spellcheck \
     devel/geany-plugin-treebrowser \
@@ -77,26 +88,13 @@ pkg install -y \
     devel/geany-themes \
     devel/gettext-tools \
     devel/git \
-    deskutils/gnome-shell-extra-extensions \
-    x11/gnome3 \
     www/iridium \
     editors/libreoffice \
     devel/libtool \
-    emulators/linux_base-c7 \
     security/sudo \
     editors/vim \
     editors/vscode \
-    net-mgmt/wifimgr \
-    x11-drivers/xf86-video-intel \
-    x11/xorg-minimal
-
-
-### Update /etc/fstab
-echo 'linprocfs   /compat/linux/proc  linprocfs       rw      0       0' >> /etc/fstab
-echo 'linsysfs    /compat/linux/sys   linsysfs        rw      0       0' >> /etc/fstab
-echo 'tmpfs    /compat/linux/dev/shm  tmpfs   rw,mode=1777    0       0' >> /etc/fstab
-mount -a
-
+    net-mgmt/wifimgr
 
 # Update robb's user
 pw group mod wheel -m robb
